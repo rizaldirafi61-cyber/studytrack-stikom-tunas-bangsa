8ab587accad57e7e376589da104a1ca11f442c49
rizaldirafi61-cyber <rizaldirafi61@gmail.com>
2026-09-09 07:14:27 -0300

    Update Supabase URL and ANON key

--- supabase[1].js
+++ supabase[1].js
@@ -1,7 +1,7 @@
 // StudyTrack STIKOM Tunas Bangsa
 // Isi dua nilai ini dari Supabase Project Settings > API.
-const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://hikiuzgeknxbmnlsqwmo.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "sb_publishable_aVRBKltM30WIzm99yeUw9A_2kGLyedC";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
